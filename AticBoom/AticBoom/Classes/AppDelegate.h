396e4705bf45481a681fdfc99fc08ea8860afcb3
Juan Francisco Rodríguez <zenilt@gmail.com>
2015-08-13 20:57:19 -0300

    Reformat and re-indent

--- AticBoom/AticBoom/Classes/AppDelegate.h
+++ AticBoom/AticBoom/Classes/AppDelegate.h
@@ -35,40 +35,39 @@
 #include "GameScene.h"
 
 
-
 /**
-@brief	The cocos2d Application.
-
-The reason for implement as private inheritance is to hide some interface call by CCDirector.
-*/
+ @brief	The cocos2d Application.
+ 
+ The reason for implement as private inheritance is to hide some interface call by CCDirector.
+ */
 class  AppDelegate : private cocos2d::CCApplication
 {
 public:
-	AppDelegate();
-	virtual ~AppDelegate();
-
+    AppDelegate();
+    virtual ~AppDelegate();
+    
     /**
-    @brief	Implement for initialize OpenGL instance, set source path, etc...
-    */
+     @brief	Implement for initialize OpenGL instance, set source path, etc...
+     */
     virtual bool initInstance();
-
+    
     /**
-    @brief	Implement CCDirector and CCScene init code here.
-    @return true    Initialize success, app continue.
-    @return false   Initialize failed, app terminate.
-    */
+     @brief	Implement CCDirector and CCScene init code here.
+     @return true    Initialize success, app continue.
+     @return false   Initialize failed, app terminate.
+     */
     virtual bool applicationDidFinishLaunching();
-
+    
     /**
-    @brief  The function be called when the application enter background
-    @param  the pointer of the application
-    */
+     @brief  The function be called when the application enter background
+     @param  the pointer of the application
+     */
     virtual void applicationDidEnterBackground();
-
+    
     /**
-    @brief  The function be called when the application enter foreground
-    @param  the pointer of the application
-    */
+     @brief  The function be called when the application enter foreground
+     @param  the pointer of the application
+     */
     virtual void applicationWillEnterForeground();
 };
 
